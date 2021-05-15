a9dae4487b2d5323d65a0c877f5388412276c79a
m081779 <marcoprincipio@hotmail.com>
2021-05-14 22:06:09 -0400

    correcting description

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', 'Welcome to Shiner\'s Hill Farms!' || '' }
+      { hid: 'description', name: 'description', content: 'Welcome to Shiner\'s Hill Farms!' || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
